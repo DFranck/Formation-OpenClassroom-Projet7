860a1b1a2481a7a838f5224a9fc61eb07b8fb05e
DFranck <franckdufournet@hotmail.fr>
2023-09-28 11:10:36 +0700

    cc createfilter menu

--- scripts/template/createFilterMenu.js
+++ scripts/template/createFilterMenu.js
@@ -1,4 +1,5 @@
 export function createFilterMenu(menu, listItems) {
+  let btnFilter= document.querySelectorAll('.filter .filter-selected button')
   let menuToFill = document.querySelector(`.${menu} ul`);
   menuToFill.innerHTML = "";
   listItems.forEach((item) => {
@@ -7,7 +8,17 @@ export function createFilterMenu(menu, listItems) {
     img.setAttribute("src", "../../assets/icons/li-cross.svg");
     li.innerHTML = item;
     li.appendChild(img);
+    li.setAttribute('class', 'list-filter')
     li.setAttribute("tabindex", 0);
     menuToFill.appendChild(li);
   });
+  btnFilter.forEach((btn)=>{
+    if (listItems.includes(btn.id))
+    menuToFill.querySelectorAll('li').forEach((li)=>{
+      if(li.textContent.includes(btn.id)){
+        li.classList.add('activeSearch')
+      }
+    })
+  })
+  console.log();
 }

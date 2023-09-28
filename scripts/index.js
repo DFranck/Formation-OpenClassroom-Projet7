1df01b7d820c7f827b9f51f5503a3ffe4a92358d
DFranck <franckdufournet@hotmail.fr>
2023-09-28 11:18:59 +0700

    cc init

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
-import { fetchRecipes } from "./api/fetchRecipes.js";
+
 import { filterRecipes } from "./functions/filterRecipes.js";
 import { toggleFilter } from "./handlers/toggleFiltersMenus.js";
 
 async function init() {
-  const recipes = await fetchRecipes();
-  filterRecipes(recipes);
+  filterRecipes();
   toggleFilter();
 }
 

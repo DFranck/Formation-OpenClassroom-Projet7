b69e5461d1365bfb045c6e59aa0ab759a53b27ea
DFranck <franckdufournet@hotmail.fr>
2023-09-28 11:14:32 +0700

    cc display recipes

--- scripts/functions/displayRecipes.js
+++ scripts/functions/displayRecipes.js
@@ -1,7 +1,9 @@
 import { createRecipeCard } from "../template/createRecipeCard.js";
 export const displayRecipes = (filteredRecipes) => {
   const recipesList = document.querySelector(".recipes-list");
-
+  const recepesNumber = document.querySelector('.recipe-number')
+  recipesList.innerHTML=''
+  recepesNumber.innerText= `${filteredRecipes.length} recettes`
   filteredRecipes.forEach((recipe) => {
     const newRecipe = createRecipeCard(recipe);
     recipesList.appendChild(newRecipe);

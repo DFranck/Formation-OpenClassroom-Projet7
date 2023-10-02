956c96c2e843f0630564220e70e42e180f959ca5
DFranck <franckdufournet@hotmail.fr>
2023-10-02 18:01:15 +0700

    a finir methode d'array

--- scripts/functions/filterRecipes.js
+++ scripts/functions/filterRecipes.js
@@ -3,87 +3,36 @@ import { displayRecipes } from "./displayRecipes.js";
 import { fetchRecipes } from "../api/fetchRecipes.js";
 
 export async function filterRecipes() {
-  const btnList = [
-    ...document.querySelectorAll(".filter .filter-selected button"),
-  ];
-  const inputValue = document.getElementById("searchBarInput").value.trim();
-  const filteredRecipesByBtn = [];
-  const filteredRecipesByInput = [];
-  let recipes = [];
-  let appliances = "";
-  let ingredientsList = "";
-  let ustensilsList = "";
-  let filteredRecipes = recipes;
-  if (recipes.length === 0) {
-    recipes = await fetchRecipes("../../data/recipes.js");
-  }
-  if (btnList.length > 0) {
-    for (let i = 0; i < recipes.length; i++) {
-      appliances = recipes[i].appliance.toLowerCase();
-      ingredientsList = recipes[i].ingredients;
-      ustensilsList = recipes[i].ustensils;
-      let matchCount = 0;
-      let j = 0;
-      while (j < btnList.length) {
-        if (appliances.includes(btnList[j].id.toLowerCase())) {
-          matchCount++;
-        }
-        j++;
+  const btnList = [...document.querySelectorAll(".filter .filter-selected button")];
+  const inputValue = document.getElementById("searchBarInput").value.trim().toLowerCase();
+  
+  let recipes = await fetchRecipes("../../data/recipes.js");
+  
+  const btnSet = new Set(btnList.map((btn) => btn.id.toLowerCase()));
+  console.log("btnSet", btnSet); // Log pour déboguer les IDs des boutons
+  
+  const filteredRecipes = recipes.filter((recipe) => {
+    
+    if (btnList.length > 0) {
+      if (!btnSet.has(recipe.appliance.toLowerCase())) {
+        return false;
       }
-      for (let i = 0; i < ingredientsList.length; i++) {
-        const ingredients = ingredientsList[i].ingredient.toLowerCase();
-        let j = 0;
-        while (j < btnList.length) {
-          if (ingredients.includes(btnList[j].id.toLowerCase())) {
-            matchCount++;
-          }
-          j++;
-        }
-      }
-      for (let i = 0; i < ustensilsList.length; i++) {
-        const ustensils = ustensilsList[i].toLowerCase();
-        let j = 0;
-        while (j < btnList.length) {
-          if (ustensils.includes(btnList[j].id.toLowerCase())) {
-            matchCount++;
-          }
-          j++;
-        }
-      }
-      if (matchCount === btnList.length) filteredRecipesByBtn.push(recipes[i]);
     }
-  }
-  if (inputValue.length >= 3) {
-    for (let i = 0; i < recipes.length; i++) {
-      const namesMatch = recipes[i].name
-        .toLowerCase()
-        .includes(inputValue.toLowerCase());
-      const descriptionsMatch = recipes[i].description
-        .toLowerCase()
-        .includes(inputValue.toLowerCase());
-      const ingredients = recipes[i].ingredients;
-      let ingredientsMatch = false;
-      for (let i = 0; i < ingredients.length; i++) {
-        const ingredientMatch = ingredients[i].ingredient
-          .toLowerCase()
-          .includes(inputValue.toLowerCase());
-        if (ingredientMatch) ingredientsMatch = true;
+    
+    if (inputValue.length >= 3) {
+      const nameMatch = recipe.name.toLowerCase().includes(inputValue);
+      const descriptionMatch = recipe.description.toLowerCase().includes(inputValue);
+      const ingredientMatch = recipe.ingredients.some((ing) => ing.ingredient.toLowerCase().includes(inputValue));
+      
+      if (!nameMatch && !descriptionMatch && !ingredientMatch) {
+        return false;
       }
-      if (namesMatch || ingredientsMatch || descriptionsMatch)
-        filteredRecipesByInput.push(recipes[i]);
     }
-  }
-  if (filteredRecipesByBtn.length > 0 && filteredRecipesByInput.length > 0) {
-    filteredRecipes = filteredRecipesByBtn.filter((recipe) =>
-      filteredRecipesByInput.includes(recipe)
-    );
-  } else if (filteredRecipesByBtn.length > 0) {
-    filteredRecipes = filteredRecipesByBtn;
-  } else if (filteredRecipesByInput.length > 0) {
-    filteredRecipes = filteredRecipesByInput;
-  } else {
-    filteredRecipes = recipes;
-  }
+    
+    return true;
+  });
+  
   displayRecipes(filteredRecipes);
   displayFiltersMenu(filteredRecipes);
 }
+

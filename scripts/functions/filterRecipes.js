961c53a0426d4e5f92fa15e3211bb373dd55ca48
DFranck <franckdufournet@hotmail.fr>
2023-10-03 14:26:51 +0700

    complexiité O(m + n * p)

--- scripts/functions/filterRecipes.js
+++ scripts/functions/filterRecipes.js
@@ -1,13 +1,7 @@
 import { displayFiltersMenu } from "./displayFiltersMenus.js";
 import { displayRecipes } from "./displayRecipes.js";
 import { fetchRecipes } from "../api/fetchRecipes.js";
-// La fonction filter a une complexité de O(n) pour chaque appel.
-// La fonction some a une complexité de O(n) dans le pire des cas (lorsqu'aucun élément ne correspond).
-// La fonction has pour un Set a une complexité moyenne de O(1).
-// En supposant que m est le nombre de boutons, n est le nombre de recettes, et p est le nombre moyen d'ingrédients et d'ustensiles par recette :
-
-// La complexité totale est approximativement :
-// O(n) * (O(1) + O(p) * O(m) + O(p) * O(m)) = O(n * (1 + 2pm))
+// O(m + n * p) = O(nb de Btn + nb de recette * nb d'ingredients/ustensils)
 export async function filterRecipes() {
   const recipes = await fetchRecipes("../../data/recipes.js");
   const btnList = [

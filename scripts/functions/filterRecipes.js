9f9e8eb424687bb1b7581c577c3b84d1ecba8ff2
DFranck <franckdufournet@hotmail.fr>
2023-10-03 12:33:02 +0700

    fin methode array

--- scripts/functions/filterRecipes.js
+++ scripts/functions/filterRecipes.js
@@ -3,36 +3,43 @@ import { displayRecipes } from "./displayRecipes.js";
 import { fetchRecipes } from "../api/fetchRecipes.js";
 
 export async function filterRecipes() {
-  const btnList = [...document.querySelectorAll(".filter .filter-selected button")];
-  const inputValue = document.getElementById("searchBarInput").value.trim().toLowerCase();
-  
-  let recipes = await fetchRecipes("../../data/recipes.js");
-  
+  const recipes = await fetchRecipes("../../data/recipes.js");
+  const btnList = [
+    ...document.querySelectorAll(".filter .filter-selected button"),
+  ];
   const btnSet = new Set(btnList.map((btn) => btn.id.toLowerCase()));
-  console.log("btnSet", btnSet); // Log pour déboguer les IDs des boutons
-  
+  const inputValue = document
+    .getElementById("searchBarInput")
+    .value.trim()
+    .toLowerCase();
   const filteredRecipes = recipes.filter((recipe) => {
-    
-    if (btnList.length > 0) {
-      if (!btnSet.has(recipe.appliance.toLowerCase())) {
-        return false;
-      }
-    }
-    
     if (inputValue.length >= 3) {
       const nameMatch = recipe.name.toLowerCase().includes(inputValue);
-      const descriptionMatch = recipe.description.toLowerCase().includes(inputValue);
-      const ingredientMatch = recipe.ingredients.some((ing) => ing.ingredient.toLowerCase().includes(inputValue));
-      
-      if (!nameMatch && !descriptionMatch && !ingredientMatch) {
-        return false;
-      }
+      const descriptionMatch = recipe.description
+        .toLowerCase()
+        .includes(inputValue);
+      const ingredientMatch = recipe.ingredients.some((ing) =>
+        ing.ingredient.toLowerCase().includes(inputValue)
+      );
+      if (!nameMatch || !descriptionMatch || !ingredientMatch) return false;
     }
-    
+    if (btnList.length > 0) {
+      const applianceMatch = btnSet.has(recipe.appliance.toLowerCase());
+      const ingredientMatch = recipe.ingredients.some((ing) =>
+        btnSet.has(ing.ingredient.toLowerCase())
+      );
+      const ustensilsMatch = recipe.ustensils.some((ustensil) =>
+        btnSet.has(ustensil.toLowerCase())
+      );
+      if (applianceMatch + ingredientMatch + ustensilsMatch < btnList.length)
+        return false;
+    }
+
     return true;
   });
-  
+  console.log(filteredRecipes);
+  console.log(btnList, btnSet);
+
   displayRecipes(filteredRecipes);
   displayFiltersMenu(filteredRecipes);
 }
-
